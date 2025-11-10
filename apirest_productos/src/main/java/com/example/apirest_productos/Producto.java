4819ea05818bc9098c304d7b4844804c3666b704
Jocude <jorge.cuevasd@gmail.com>
2025-11-10 22:56:19 +0100

    ADD: Tabla productos creada desde el proyecto

---
+++ apirest_productos/src/main/java/com/example/apirest_productos/Producto.java
@@ -0,0 +1,74 @@
+package com.example.apirest_productos;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //UTILIZA id COMO PK Y LE ASIGNA VALOR SECUENCIAL
+    Integer id;
+    String nombre;
+    String detalle;
+    BigDecimal precio;
+    @CreationTimestamp // ASIGNA FECHA ACTUAL DE CREACION
+    LocalDateTime fechaCreado;
+    @UpdateTimestamp // ASIGNA FECHA ACTUALIZACION
+    LocalDateTime fechaActualizado;
+
+    public Producto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public BigDecimal getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(BigDecimal precio) {
+        this.precio = precio;
+    }
+
+    public LocalDateTime getFechaCreado() {
+        return fechaCreado;
+    }
+
+    public void setFechaCreado(LocalDateTime fechaCreado) {
+        this.fechaCreado = fechaCreado;
+    }
+
+    public LocalDateTime getFechaActualizado() {
+        return fechaActualizado;
+    }
+
+    public void setFechaActualizado(LocalDateTime fechaActualizado) {
+        this.fechaActualizado = fechaActualizado;
+    }
+}
